cb8b0ddb6faf7ab6c00a54bb96195add5324309d
nicolynk03 <kwoknicolyn@gmail.com>
2025-08-15 15:57:59 +1000

    wip on theme switches

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,23 @@
 // mode/theme switch function
 function switchTheme() {
+    const root = document.documentElement;
     const currentTheme = document.documentElement.getAttribute("data-theme");
-    document.documentElement.setAttribute("data-theme", currentTheme == "dark" ? "light" : "dark");
+    const newTheme = currentTheme == "dark" ? "light" : "dark";
+    root.setAttribute("data-theme", newTheme);
+    // document.documentElement.setAttribute("data-theme", currentTheme == "dark" ? "light" : "dark");
+
+    // update button icon
+    const themeBtn = document.getElementById("theme-toggle");
+    if (newTheme === "dark") {
+        themeBtn.innerHTML = '🌙';
+    } else {
+        themeBtn.innerHTML = '☀️';
+    }
 }
+
+// initialise icon on page load
+window.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme") || "light";
+    const themeBtn = document.getElementById("theme-toggle");
+    themeBtn.innerHTML = currentTheme === "dark" ? "🌙" : '☀️';
+});
