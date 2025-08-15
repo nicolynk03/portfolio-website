fa29afdd9def5edc47e4afff9517aabd2a452c90
nicolynk03 <kwoknicolyn@gmail.com>
2025-08-15 16:08:49 +1000

    theme persist even after the page is refreshed, added visual feedback to theme emoji icon when hovered, made some changes to font colour in experience card.

--- JS/script.js
+++ JS/script.js
@@ -6,17 +6,20 @@ function switchTheme() {
     root.setAttribute("data-theme", newTheme);
     // document.documentElement.setAttribute("data-theme", currentTheme == "dark" ? "light" : "dark");
 
-    // update button icon
+    // save current theme
+    localStorage.setItem("theme", newTheme);
+
+    // update button icon according to current theme
     const themeBtn = document.getElementById("theme-toggle");
-    if (newTheme === "dark") {
-        themeBtn.innerHTML = '🌙';
-    } else {
-        themeBtn.innerHTML = '☀️';
-    }
+    themeBtn.innerHTML = newTheme === "dark" ? '🌙' : '☀️';
 }
 
 // initialise icon on page load
 window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    document.documentElement.setAttribute("data-theme", savedTheme);
+
+
     const currentTheme = document.documentElement.getAttribute("data-theme") || "light";
     const themeBtn = document.getElementById("theme-toggle");
     themeBtn.innerHTML = currentTheme === "dark" ? "🌙" : '☀️';
